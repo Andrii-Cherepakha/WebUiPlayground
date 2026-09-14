6df19e15bad9fbce68be9bd6ea872e03a1d883b4
Andrii Cherepakha <andrey.cherepakha@gmail.com>
2026-09-14 22:08:40 +0200

    Playwrigth Frame test

---
+++ PlayWrightTests/PageObjects/UiTestingPlayground/FramesPage.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Playwright;
+
+namespace PlayWrightTests.PageObjects.UiTestingPlayground
+{
+    public class FramesPage : BasePage
+    {
+        public FramesPage(IPage page) : base(page) { }
+
+        private IFrameLocator OuterFrameLocator => Page.FrameLocator("#frame-outer");
+        private IFrameLocator InnerFrameLocator => OuterFrameLocator.FrameLocator("#frame-inner");
+
+        public FrameWithButtons OuterFrame => new FrameWithButtons(OuterFrameLocator);
+        public FrameWithButtons InnerFrame => new FrameWithButtons(InnerFrameLocator);
+
+
+        public class FrameWithButtons
+        {
+            private readonly IFrameLocator frame;
+            public FrameWithButtons(IFrameLocator frame)
+            {
+                this.frame = frame;
+            }
+
+            public ILocator EditBtn => frame.Locator("button[data-action='edit']");
+            public ILocator SubmitBtn => frame.GetByText("Submit");
+            public ILocator ClickMeBtn => frame.Locator("button[name='my-button']");
+            public ILocator PrimaryBtn => frame.Locator("button.btn-class");
+            public ILocator Result => frame.Locator("#result");
+        }
+    }
+}
---
+++ PlayWrightTests/TestCases/UiTestingPlayground/FrameTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using PlayWrightTests.PageObjects.UiTestingPlayground;
+
+namespace PlayWrightTests.TestCases.UiTestingPlayground
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class FrameTests : BaseTest
+    {
+        [Test]
+        public async Task OuterFrameTest()
+        {
+            var homePage = GetPage<HomePage>();
+            await homePage.OpenAsync();
+            await homePage.OpenSectionAsync("Frames");
+
+            var framesPage = GetPage<FramesPage>();
+
+            await framesPage.OuterFrame.EditBtn.ClickAsync();
+            Assert.That(await framesPage.OuterFrame.Result.InnerTextAsync(), Is.EqualTo("Button pressed: Edit"));
+
+            await framesPage.OuterFrame.SubmitBtn.ClickAsync();
+            Assert.That(await framesPage.OuterFrame.Result.InnerTextAsync(), Is.EqualTo("Button pressed: Submit"));
+
+            await framesPage.OuterFrame.ClickMeBtn.ClickAsync();
+            Assert.That(await framesPage.OuterFrame.Result.InnerTextAsync(), Is.EqualTo("Button pressed: Click me"));
+
+            await framesPage.OuterFrame.PrimaryBtn.ClickAsync();
+            Assert.That(await framesPage.OuterFrame.Result.InnerTextAsync(), Is.EqualTo("Button pressed: Primary"));
+        }
+
+        [Test]
+        public async Task InnerFrameTest()
+        {
+            var homePage = GetPage<HomePage>();
+            await homePage.OpenAsync();
+            await homePage.OpenSectionAsync("Frames");
+
+            var framesPage = GetPage<FramesPage>();
+
+            await framesPage.InnerFrame.EditBtn.ClickAsync();
+            Assert.That(await framesPage.InnerFrame.Result.InnerTextAsync(), Is.EqualTo("Button pressed: Edit"));
+
+            await framesPage.InnerFrame.SubmitBtn.ClickAsync();
+            Assert.That(await framesPage.InnerFrame.Result.InnerTextAsync(), Is.EqualTo("Button pressed: Submit"));
+
+            await framesPage.InnerFrame.ClickMeBtn.ClickAsync();
+            Assert.That(await framesPage.InnerFrame.Result.InnerTextAsync(), Is.EqualTo("Button pressed: Click me"));
+
+            await framesPage.InnerFrame.PrimaryBtn.ClickAsync();
+            Assert.That(await framesPage.InnerFrame.Result.InnerTextAsync(), Is.EqualTo("Button pressed: Primary"));
+        }
+    }
+}
