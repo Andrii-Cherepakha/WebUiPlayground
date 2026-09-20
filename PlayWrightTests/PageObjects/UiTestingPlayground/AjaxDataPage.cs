96c50a7465f9338d91645687178751244fb4fcd5
Andrii Cherepakha <andrey.cherepakha@gmail.com>
2026-09-20 22:21:51 +0200

    Playwright Network Mocking Tests

---
+++ PlayWrightTests/PageObjects/UiTestingPlayground/AjaxDataPage.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Playwright;
+
+namespace PlayWrightTests.PageObjects.UiTestingPlayground
+{
+    public class AjaxDataPage : BasePage
+    {
+        public AjaxDataPage(IPage page) : base(page) { }
+
+        public ILocator AjaxButton => Page.Locator("#ajaxButton");
+
+        private const string AjaxDataUrl = "**/ajaxdata";
+
+        public async Task WaitForSpinner()
+        {
+            await Page.Locator("#spinner").WaitForAsync(new() { State = WaitForSelectorState.Hidden });
+        }
+
+        public async Task<IEnumerable<string>> GetContent()
+        {
+            var items = await Page.Locator("#content p").AllAsync();
+            var response = new List<string>();
+            foreach (var item in items)
+            {
+                var text = await item.InnerTextAsync();
+                response.Add(text);
+            }
+            return response;
+        }
+
+        public async Task MockResponse(string text)
+        {
+            await Page.RouteAsync(AjaxDataUrl, async route =>
+            {
+                await route.FulfillAsync(new()
+                {
+                    Status = 200,
+                    ContentType = "text/plain",
+                    Body = text
+                });
+            });
+        }
+
+        public async Task UnmockResponse()
+        {
+            await Page.UnrouteAsync(AjaxDataUrl);
+        }
+    }
+}
---
+++ PlayWrightTests/TestCases/UiTestingPlayground/NetworkMockingTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using PlayWrightTests.PageObjects.UiTestingPlayground;
+
+namespace PlayWrightTests.TestCases.UiTestingPlayground
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class NetworkMockingTests : BaseTest
+    {
+        [Test]
+        public async Task AjaxDataMocking()
+        {
+            var homePage = GetPage<HomePage>();
+            await homePage.OpenAsync();
+
+            await homePage.OpenSectionAsync("AJAX Data");
+
+            var ajaxDataPage = GetPage<AjaxDataPage>();
+
+            await ajaxDataPage.AjaxButton.ClickAsync();
+            await ajaxDataPage.WaitForSpinner();
+            Assert.That(await ajaxDataPage.GetContent(), Does.Contain("Data loaded with AJAX get request."));
+
+            await ajaxDataPage.MockResponse("Mocked response text");
+
+            await ajaxDataPage.AjaxButton.ClickAsync();
+            await ajaxDataPage.WaitForSpinner();
+            Assert.That(await ajaxDataPage.GetContent(), Does.Contain("Mocked response text"));
+
+            await ajaxDataPage.UnmockResponse();
+
+            await ajaxDataPage.AjaxButton.ClickAsync();
+            await ajaxDataPage.WaitForSpinner();
+            Assert.That(await ajaxDataPage.GetContent(), Has.Exactly(2).EqualTo("Data loaded with AJAX get request."));
+            Assert.That(await ajaxDataPage.GetContent(), Has.Exactly(1).EqualTo("Mocked response text"));
+        }
+    }
+}
